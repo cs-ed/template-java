3a005372779574686ac78d109243093dbb125f61
Kevin Buffardi <kbuffardi@csuchico.edu>
2021-10-04 12:14:18 -0700

    Create main.java with scanner for input

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner; // use Scanner for user input
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+
+    // get two numbers from the user:
+    int first_number = scan.nextInt();
+    int second_number = scan.nextInt();
+    
+    // TODO: calculate division, or give an error if dividing by zero
+      
+  }
+}
